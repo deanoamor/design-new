99cb40fc5ceb1e212a80dabd3137bd31d277f8fd
deanoamor <toonoob4@gmail.com>
2023-08-11 21:24:25 +0700

    fix footer

--- resources/views/main/member/templates/member-footer.blade.php
+++ resources/views/main/member/templates/member-footer.blade.php
@@ -1,27 +1,37 @@
-<div class="navbar navbar-expand-lg mt-5" style="background-color: #4F7AF6;">
+<footer class="text-center text-lg-start text-white mt-5" style="background-color: #4F7AF6;">
+    <!-- Section: Social media -->
+    <div class="container">
+        <section class="d-flex justify-content-between p-4" style="background-color: #4F7AF6;">
+            <!-- Left -->
 
-    <div class="container" style="color: #fff;">
-        <p style="margin-top: 20px ;">Copyright Ⓒ 2023 <b>Norin</b>
-        </p>
+            <div class="me-5">
+                <span>Copyright Ⓒ 2023 <b>Norin</b></span>
+            </div>
+            <!-- Left -->
 
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav mb-2 mb-lg-0 me-5">
-                <li class="nav-item me-3">
-                    <i class="fa-brands fa-twitter"></i>
-                </li>
-                <li class="nav-item me-3">
+            <!-- Right -->
+            <div class="">
+                <a href="" class="text-white me-4">
                     <i class="fab fa-facebook-f"></i>
-                </li>
-                <li class="nav-item me-3">
-                    <i class="fa-brands fa-linkedin"></i>
-                </li>
-                <li class="nav-item me-3">
-                    <i class="fa-solid fa-envelope"></i>
-                </li>
-                <li class="nav-item me-3">
-                    <i class="fa-brands fa-instagram"></i>
-                </li>
-            </ul>
-        </div>
+                </a>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-google"></i>
+                </a>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-github"></i>
+                </a>
+            </div>
+            <!-- Right -->
+        </section>
     </div>
-</div>
+
+</footer>
